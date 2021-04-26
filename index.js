50f799216c5b653d3f60433d9f87b4ebedbfd503
Arif-hossaian <arifhossain42136@gmail.com>
2021-04-26 12:47:40 +0600

    deploying to heroku

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// connecting to server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
